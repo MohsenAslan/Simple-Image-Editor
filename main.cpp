65505f462e31bc8e4c07eed6dc052b7f225d2b04
Mohsen Aslan <mohsen.mohamed.aslan@gmail.com>
2025-09-29 16:07:02 +0300

    Add filter 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include "Image_Class.h"
 using namespace std;
 
+
+//filter11 resize 
 viod resize (Image &img ,int newWidth,int newHeight ){
 
     Image resized(newWidth, newHeight);
@@ -20,6 +22,28 @@ viod resize (Image &img ,int newWidth,int newHeight ){
         }
     }
     img = resized;
+}
+
+//filter2 black&white
+void blackWhite(Image &img) {
+    for (int i = 0; i < img.width; i++) {
+        for (int j = 0; j < img.height; j++) {
+            unsigned avg = 0;
+            
+            for (int k = 0; k < img.channels; k++) {
+                avg += img(i, j, k);
+            }
+
+            avg = avg / img.channels;
+            
+            unsigned correct = (avg > 128) ? 255 : 0;
+
+            for (int k = 0; k < img.channels; k++) {
+                img(i, j, k) = correct;
+            }
+        }
+    }
+    img=blackWhite
 }
 
 int main() {
