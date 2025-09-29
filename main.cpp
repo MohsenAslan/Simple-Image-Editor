f92988fadcfcdf98e7c53908f53b3f0469fb7001
Mohsen Aslan <mohsen.mohamed.aslan@gmail.com>
2025-09-29 16:31:37 +0300

    Add filter 5

--- main.cpp
+++ main.cpp
@@ -24,9 +24,12 @@ viod resize (Image &img ,int newWidth,int newHeight ){
     img = resized;
 }
 
+
 //filter2 black&white
 void blackWhite(Image &img) {
+    
     for (int i = 0; i < img.width; i++) {
+        
         for (int j = 0; j < img.height; j++) {
             unsigned avg = 0;
             
@@ -43,8 +46,41 @@ void blackWhite(Image &img) {
             }
         }
     }
-    img=blackWhite
+    img = blackWhite;
 }
+
+// filter5 flip (H)
+void flipHorizontal(Image &img) {
+    
+    for (int i = 0; i < img.width / 2; i++) {
+        
+        for (int j = 0; j < img.height; j++) {
+            
+            for (int k = 0; k < img.channels; k++) {
+                
+                swap(img(i, j, k), img(img.width - 1 - i, j, k));
+            }
+        }
+    }
+    img = flipHorizontal;
+}
+//(v)
+void flipVertical(Image &img) {
+     for (int i = 0; i < img.width; i++) {
+         
+        for (int j = 0; j < img.height / 2; j++) {
+            
+            for (int k = 0; k < img.channels; k++) {
+                
+                swap(img(i, j, k), img(i, img.height - 1 - j, k));
+            }
+        }
+    }
+    img = flipVertical;
+}
+
+
+
 
 int main() {
   
